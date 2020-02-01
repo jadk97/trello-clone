3576e4d55df6ce087a2df261990dd5b1d55405ca
Jad K <jad_dis@hotmail.com>
2020-02-01 04:00:37 -0500

    setup the basic folder structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrelloList from "./TrelloList";
+
+function App() {
+  return (
+    <div className="App"> 
+    <p>This is my app component!</p>
+    <TrelloList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TrelloList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TrelloList = () => {
+  return (
+    <h2>
+    List
+    </h2>
+  )
+}
+
+export default TrelloList;
